5a45bc505f372f40de1fc51d14bee394d9a55e33
AleManni <ale_manni@mail.com>
2021-02-13 10:38:17 +0000

    part of previous commit

--- ToDoList/ViewModel/ListModelSection.swift
+++ ToDoList/ViewModel/ListModelSection.swift
@@ -1,10 +1,6 @@
 import Foundation
 
-enum Sections: Hashable, CaseIterable {
-  case Monday, Tuesday, Wednesday, Thursday, Friday
-}
-
-struct ListModelSection:Hashable{
+struct ListModelSection: Hashable {
     let sectionName: String
     var items: [ListModel]
 }
